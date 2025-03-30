e1e2c3ac906b407e336553715e1653fcdc25a2bc
Kirill Klochkov <klochkov.kirill@gmail.com>
2025-03-30 22:52:57 +0200

    add packed_buffer and contiguous_storage classes

---
+++ sw_renderer/contiguous_storage.h
@@ -0,0 +1,161 @@
+#pragma once
+
+#include <cassert>
+#include <memory>
+
+namespace rtw::stl
+{
+
+template <typename T>
+class AlignedObjectStorage
+{
+public:
+  static_assert(std::is_standard_layout_v<T>, " AlignedObjectStorage<T> requires T to be standard layout.");
+  static_assert(std::is_trivially_copyable_v<T>, " AlignedObjectStorage<T> requires T to be trivially copyable.");
+  static_assert(std::is_trivially_destructible_v<T>,
+                " AlignedObjectStorage<T> requires T to be trivially destructible.");
+
+  using value_type = T;
+  using reference = value_type&;
+  using const_reference = const value_type&;
+  using pointer = value_type*;
+  using const_pointer = const value_type*;
+
+  constexpr AlignedObjectStorage() : constructed_{false} {}
+  constexpr AlignedObjectStorage(const AlignedObjectStorage&) = default;
+  constexpr AlignedObjectStorage(AlignedObjectStorage&&) = default;
+  constexpr AlignedObjectStorage& operator=(const AlignedObjectStorage&) = default;
+  constexpr AlignedObjectStorage& operator=(AlignedObjectStorage&&) = default;
+  ~AlignedObjectStorage() { destruct(); }
+
+  constexpr bool is_constructed() const { return constructed_; }
+
+  template <typename... ArgsT>
+  constexpr void construct(ArgsT&&... args)
+  {
+    assert(!is_constructed());
+    // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+    ::new (reinterpret_cast<void*>(&data_[0U])) T{std::forward<ArgsT>(args)...};
+    constructed_ = true;
+  }
+
+  constexpr T& construct_for_overwrite_at()
+  {
+    assert(!is_constructed());
+    // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast, cppcoreguidelines-owning-memory)
+    auto* value = ::new (reinterpret_cast<void*>(&data_[0U])) T;
+    constructed_ = true;
+    return *value;
+  }
+
+  constexpr void destruct()
+  {
+    if (is_constructed())
+    {
+      get_pointer()->~T();
+      constructed_ = false;
+    }
+  }
+
+  constexpr pointer get_pointer()
+  {
+    assert(is_constructed());
+    return std::launder(reinterpret_cast<T*>(&data_[0U])); // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast)
+  }
+
+  constexpr reference get_reference() { return *get_pointer(); }
+
+private:
+  alignas(alignof(T)) std::byte data_[sizeof(T)]{};
+  bool constructed_ : 1;
+};
+
+template <typename T>
+class ContiguousStorage
+{
+  using AlignedStorage = AlignedObjectStorage<T>;
+
+public:
+  static_assert(std::is_standard_layout_v<T>, " ContiguousStorage<T> requires T to be standard layout.");
+  static_assert(std::is_trivially_copyable_v<T>, " ContiguousStorage<T> requires T to be trivially copyable.");
+  static_assert(std::is_trivially_destructible_v<T>, " ContiguousStorage<T> requires T to be trivially destructible.");
+
+  using value_type = typename AlignedStorage::value_type;
+  using size_type = std::size_t;
+  using reference = typename AlignedStorage::reference;
+  using const_reference = typename AlignedStorage::const_reference;
+  using pointer = typename AlignedStorage::pointer;
+  using const_pointer = typename AlignedStorage::const_pointer;
+
+  explicit ContiguousStorage(const size_type capacity)
+      : data_{std::make_unique<AlignedStorage[]>(capacity)}, capacity_{capacity}
+  {
+    assert(capacity > 0U);
+  }
+
+  ContiguousStorage(const ContiguousStorage&) = delete;
+  ContiguousStorage(ContiguousStorage&&) = default;
+  ContiguousStorage& operator=(const ContiguousStorage&) = delete;
+  ContiguousStorage& operator=(ContiguousStorage&&) = default;
+  ~ContiguousStorage() { clear(); }
+
+  size_type used_slots() const { return used_slots_; }
+  bool empty() const { return used_slots_ == 0U; }
+  size_type capacity() const { return capacity_; }
+
+  template <typename... ArgsT>
+  void construct_at(const size_type index, ArgsT&&... args)
+  {
+    assert(index < capacity_);
+    data_[index].construct(std::forward<ArgsT>(args)...);
+    ++used_slots_;
+  }
+
+  T& construct_for_overwrite_at(const size_type index)
+  {
+    assert(index < capacity_);
+    auto& value = data_[index].construct_for_overwrite_at();
+    ++used_slots_;
+    return value;
+  }
+
+  void destruct_at(const size_type index)
+  {
+    assert(index < capacity_);
+    assert(!empty());
+    data_[index].destruct();
+    --used_slots_;
+  }
+
+  bool is_constructed(const size_type index) const { return data_[index].is_constructed(); }
+
+  pointer get_pointer(const size_type index)
+  {
+    assert(index < used_slots_);
+    return data_[index].get_pointer();
+  }
+
+  reference get_reference(const size_type index) { return *get_pointer(index); }
+
+  reference operator[](const size_type index) { return get_reference(index); }
+  const_reference operator[](const size_type index) const { return get_reference(index); }
+
+  void clear()
+  {
+    for (size_type index = 0U; index < capacity_; ++index)
+    {
+      data_[index].destruct();
+    }
+    used_slots_ = 0U;
+  }
+
+  pointer data() { return get_pointer(0U); }
+  const_pointer data() const { return data(); }
+
+private:
+  std::unique_ptr<AlignedStorage[]> data_;
+  size_type used_slots_{0U};
+  size_type capacity_{0U};
+};
+
+} // namespace rtw::stl
---
+++ sw_renderer/tests/packed_buffer_test.cpp
@@ -0,0 +1,175 @@
+#include "sw_renderer/packed_buffer.h"
+
+#include <gtest/gtest.h>
+
+namespace
+{
+
+struct Struct
+{
+  Struct() = default;
+  Struct(const float a, const std::int32_t b, const std::uint8_t c) : a{a}, b{b}, c{c} {}
+
+  float a{};
+  std::int32_t b{};
+  std::uint8_t c{};
+
+  bool operator==(const Struct& other) const { return std::tie(a, b, c) == std::tie(other.a, other.b, other.c); }
+};
+
+using PackedBuffer = rtw::stl::PackedBuffer<Struct>;
+
+} // namespace
+
+TEST(PackedBufferTest, constructor)
+{
+  PackedBuffer buffer{10U};
+  EXPECT_EQ(buffer.size(), 0U);
+  EXPECT_EQ(buffer.capacity(), 10U);
+  EXPECT_TRUE(buffer.empty());
+
+  EXPECT_DEATH(PackedBuffer{0U}, ".*");
+}
+
+TEST(PackedBufferTest, push_back)
+{
+  PackedBuffer buffer{10U};
+  EXPECT_EQ(buffer.size(), 0U);
+  EXPECT_EQ(buffer.capacity(), 10U);
+  EXPECT_TRUE(buffer.empty());
+
+  for (std::size_t i = 0U; i < buffer.capacity(); ++i)
+  {
+    const Struct s{static_cast<float>(i + 1U), static_cast<std::int32_t>(i + 1U), static_cast<std::uint8_t>(i + 1U)};
+    buffer.push_back(s);
+    EXPECT_EQ(buffer[i], s);
+  }
+
+  EXPECT_EQ(buffer.size(), buffer.capacity());
+
+  {
+    for (std::size_t i = 0U; i < buffer.size(); ++i)
+    {
+      EXPECT_EQ(buffer[i], (Struct{static_cast<float>(i + 1U), static_cast<std::int32_t>(i + 1U),
+                                   static_cast<std::uint8_t>(i + 1U)}));
+    }
+  }
+
+  EXPECT_DEATH(buffer.push_back(Struct{}), ".*");
+
+  buffer.clear();
+  EXPECT_EQ(buffer.size(), 0U);
+  EXPECT_EQ(buffer.capacity(), 10U);
+  EXPECT_TRUE(buffer.empty());
+}
+
+TEST(PackedBufferTest, emplace_back)
+{
+  PackedBuffer buffer{10U};
+  EXPECT_EQ(buffer.size(), 0U);
+  EXPECT_EQ(buffer.capacity(), 10U);
+  EXPECT_TRUE(buffer.empty());
+
+  for (std::size_t i = 0U; i < buffer.capacity(); ++i)
+  {
+    const auto& s = buffer.emplace_back(static_cast<float>(i + 1U), static_cast<std::int32_t>(i + 1U),
+                                        static_cast<std::uint8_t>(i + 1U));
+    EXPECT_EQ(buffer[i], s);
+    EXPECT_EQ(buffer[i], (Struct{static_cast<float>(i + 1U), static_cast<std::int32_t>(i + 1U),
+                                 static_cast<std::uint8_t>(i + 1U)}));
+  }
+
+  EXPECT_EQ(buffer.size(), buffer.capacity());
+
+  {
+    for (std::size_t i = 0U; i < buffer.size(); ++i)
+    {
+      EXPECT_EQ(buffer[i], (Struct{static_cast<float>(i + 1U), static_cast<std::int32_t>(i + 1U),
+                                   static_cast<std::uint8_t>(i + 1U)}));
+    }
+  }
+
+  EXPECT_DEATH(buffer.emplace_back(), ".*");
+
+  buffer.clear();
+  EXPECT_EQ(buffer.size(), 0U);
+  EXPECT_EQ(buffer.capacity(), 10U);
+  EXPECT_TRUE(buffer.empty());
+}
+
+TEST(PackedBufferTest, remove)
+{
+  PackedBuffer buffer{10U};
+  EXPECT_EQ(buffer.size(), 0U);
+  EXPECT_EQ(buffer.capacity(), 10U);
+  EXPECT_TRUE(buffer.empty());
+
+  // Remove from the head of the buffer.
+  {
+    for (std::size_t i = 0U; i < buffer.capacity(); ++i)
+    {
+      buffer.emplace_back(static_cast<float>(i + 1U), static_cast<std::int32_t>(i + 1U),
+                          static_cast<std::uint8_t>(i + 1U));
+    }
+
+    const auto size = buffer.size() - 1U;
+    for (std::size_t i = 0U; i < size; ++i)
+    {
+      const std::size_t index = 0U;
+      const auto* ptr = &buffer[index];
+
+      buffer.remove(index);
+
+      EXPECT_EQ(buffer.size(), buffer.capacity() - i - 1U);
+      EXPECT_FALSE(buffer.empty());
+      EXPECT_EQ(buffer[index],
+                (Struct{static_cast<float>(buffer.size() + 1U), static_cast<std::int32_t>(buffer.size() + 1U),
+                        static_cast<std::uint8_t>(buffer.size() + 1U)}));
+      EXPECT_EQ(ptr, &buffer[index]);
+    }
+
+    EXPECT_EQ(buffer.size(), 1U);
+    EXPECT_FALSE(buffer.empty());
+    EXPECT_EQ(buffer[0U], (Struct{2.0F, 2, 2}));
+
+    buffer.remove(0U);
+
+    EXPECT_EQ(buffer.size(), 0U);
+    EXPECT_TRUE(buffer.empty());
+    EXPECT_DEATH(buffer.remove(0U), ".*");
+  }
+
+  // Remove from the tail of the buffer.
+  {
+    for (std::size_t i = 0U; i < buffer.capacity(); ++i)
+    {
+      buffer.emplace_back(static_cast<float>(i + 1U), static_cast<std::int32_t>(i + 1U),
+                          static_cast<std::uint8_t>(i + 1U));
+    }
+
+    const auto size = buffer.size() - 1U;
+    for (std::size_t i = 0U; i < size; ++i)
+    {
+      const std::size_t index = buffer.size() - 2U;
+      const auto* ptr = &buffer[index];
+
+      buffer.remove(buffer.size() - 1U);
+
+      EXPECT_EQ(buffer.size(), buffer.capacity() - i - 1U);
+      EXPECT_FALSE(buffer.empty());
+      EXPECT_EQ(buffer[index], (Struct{static_cast<float>(index + 1U), static_cast<std::int32_t>(index + 1U),
+                                       static_cast<std::uint8_t>(index + 1U)}));
+      EXPECT_EQ(ptr, &buffer[index]);
+    }
+
+    EXPECT_EQ(buffer.size(), 1U);
+    EXPECT_FALSE(buffer.empty());
+    EXPECT_EQ(buffer[0U], (Struct{1.0F, 1, 1}));
+
+    buffer.remove(0U);
+
+    EXPECT_EQ(buffer.size(), 0U);
+    EXPECT_TRUE(buffer.empty());
+    EXPECT_DEATH(buffer.remove(0U), ".*");
+  }
+}
