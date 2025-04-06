2842b70f09bd9e6c30aa0729bb9565ec8bff2a08
Kirill Klochkov <759108+kklochkov@users.noreply.github.com>
2025-04-06 21:51:13 +0200

    add circular_buffer

--- sw_renderer/circular_buffer.h
+++ sw_renderer/circular_buffer.h
@@ -5,39 +5,8 @@
 namespace rtw::stl
 {
 
-/// Class keeps instances of T in a contiguous memory block tightly packed.
-/// Avoiding memory fragmentation is achieve by reserving a fixed size of memory.
-/// Upon removing an element, the last element is moved to the removed element's position.
-/// For example, if the buffer contains 5 elements and the 3rd element is removed,
-/// the 5th element is moved to the 3rd position.
-///
-/// Case 1, removing from the head of the buffer:
-/// a = {1, 2, 3, 4, 5}
-/// - remove(0) => a = {5, 2, 3, 4}
-/// - remove(0) => a = {4, 2, 3}
-/// - remove(0) => a = {3, 2}
-/// - remove(0) => a = {2}
-/// - remove(0) => a = {}
-///
-/// Case 2, removing from the tail of the buffer:
-/// a = {1, 2, 3, 4, 5}
-/// - remove(4) => a = {1, 2, 3, 4}
-/// - remove(3) => a = {1, 2, 3}
-/// - remove(2) => a = {1, 2}
-/// - remove(1) => a = {1}
-/// - remove(0) => a = {}
-///
-/// Case 3, removing from the middle of the buffer:
-/// a = {1, 2, 3, 4, 5}
-/// - remove(2) => a = {1, 2, 5, 4}
-/// - remove(2) => a = {1, 2, 4}
-/// - remove(1) => a = {1, 4}
-/// - remove(0) => a = {4}
-/// - remove(0) => a = {}
-///
-/// @tparam T Type of the element to be stored in the buffer.
 template <typename T>
-class PackedBuffer
+class CircularBuffer
 {
   using StorageType = ContiguousStorage<T>;
 
@@ -51,7 +20,7 @@ public:
   using iterator = typename StorageType::iterator;
   using const_iterator = typename StorageType::const_iterator;
 
-  explicit PackedBuffer(const size_type capacity) : storage_{capacity} {}
+  explicit CircularBuffer(const size_type capacity) : storage_{capacity} {}
 
   size_type size() const { return storage_.used_slots(); }
   bool empty() const { return storage_.empty(); }
@@ -60,7 +29,12 @@ public:
   template <typename... ArgsT>
   reference emplace_back(ArgsT&&... args)
   {
-    return storage_.construct_at(size(), std::forward<ArgsT>(args)...);
+    const auto index = (head_++) % capacity();
+    if (storage_.is_constructed(index))
+    {
+      storage_.destruct_at(index);
+    }
+    return storage_.construct_at(index, std::forward<ArgsT>(args)...);
   }
 
   template <typename U = T>
@@ -75,15 +49,11 @@ public:
     storage_.destruct_at(size() - 1U);
   }
 
-  void remove(const typename ContiguousStorage<T>::size_type index)
+  void clear()
   {
-    assert(index < size());
-    const auto last_index = size() - 1U;
-    std::exchange(storage_[index], storage_[last_index]);
-    storage_.destruct_at(last_index);
+    head_ = 0U;
+    storage_.clear();
   }
-
-  void clear() { storage_.clear(); }
 
   reference operator[](const size_type index) { return storage_[index]; }
   const_reference operator[](const size_type index) const { return storage_[index]; }
@@ -98,6 +68,7 @@ public:
 
 private:
   StorageType storage_;
+  size_type head_{0U};
 };
 
 } // namespace rtw::stl
