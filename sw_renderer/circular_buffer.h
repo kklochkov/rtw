51d0be02ba4de8e93462aabedafea84abad2eebd
Kirill Klochkov <759108+kklochkov@users.noreply.github.com>
2025-04-07 23:08:09 +0200

    add queue

--- sw_renderer/circular_buffer.h
+++ sw_renderer/circular_buffer.h
@@ -20,14 +20,14 @@ public:
   using iterator = typename StorageType::iterator;
   using const_iterator = typename StorageType::const_iterator;
 
-  explicit CircularBuffer(const size_type capacity) : storage_{capacity} {}
+  explicit CircularBuffer(const size_type capacity) noexcept : storage_{capacity} {}
 
-  size_type size() const { return storage_.used_slots(); }
-  bool empty() const { return storage_.empty(); }
-  size_type capacity() const { return storage_.capacity(); }
+  size_type size() const noexcept { return storage_.used_slots(); }
+  bool empty() const noexcept { return storage_.empty(); }
+  size_type capacity() const noexcept { return storage_.capacity(); }
 
   template <typename... ArgsT>
-  reference emplace_back(ArgsT&&... args)
+  reference emplace_back(ArgsT&&... args) noexcept
   {
     const auto index = (head_++) % capacity();
     if (storage_.is_constructed(index))
@@ -38,33 +38,33 @@ public:
   }
 
   template <typename U = T>
-  void push_back(U&& value)
+  void push_back(U&& value) noexcept
   {
     emplace_back(std::forward<U>(value));
   }
 
-  void pop_back()
+  void pop_back() noexcept
   {
     assert(!empty());
     storage_.destruct_at(size() - 1U);
   }
 
-  void clear()
+  void clear() noexcept
   {
     head_ = 0U;
     storage_.clear();
   }
 
-  reference operator[](const size_type index) { return storage_[index]; }
-  const_reference operator[](const size_type index) const { return storage_[index]; }
+  reference operator[](const size_type index) noexcept { return storage_[index]; }
+  const_reference operator[](const size_type index) const noexcept { return storage_[index]; }
 
-  iterator begin() { return storage_.begin(); }
-  const_iterator begin() const { return storage_.begin(); }
-  const_iterator cbegin() const { return storage_.cbegin(); }
+  iterator begin() noexcept { return storage_.begin(); }
+  const_iterator begin() const noexcept { return storage_.begin(); }
+  const_iterator cbegin() const noexcept { return storage_.cbegin(); }
 
-  iterator end() { return storage_.end(); }
-  const_iterator end() const { return storage_.end(); }
-  const_iterator cend() const { return storage_.cend(); }
+  iterator end() noexcept { return storage_.end(); }
+  const_iterator end() const noexcept { return storage_.end(); }
+  const_iterator cend() const noexcept { return storage_.cend(); }
 
 private:
   StorageType storage_;
---
+++ sw_renderer/tests/queue_test.cpp
@@ -0,0 +1,107 @@
+#include "sw_renderer/queue.h"
+
+#include <gtest/gtest.h>
+
+namespace
+{
+
+struct Struct
+{
+  Struct() = default;
+  Struct(const float a, const std::int32_t b, const std::uint8_t c) : a{a}, b{b}, c{c} {}
+
+  float a{};
+  std::int32_t b{};
+  std::uint8_t c{};
+
+  bool operator==(const Struct& other) const { return std::tie(a, b, c) == std::tie(other.a, other.b, other.c); }
+};
+
+using Queue = rtw::stl::Queue<Struct>;
+
+} // namespace
+
+TEST(QueueTest, constructor)
+{
+  Queue queue{10U};
+  EXPECT_EQ(queue.size(), 0U);
+  EXPECT_EQ(queue.capacity(), 10U);
+  EXPECT_TRUE(queue.empty());
+
+  EXPECT_DEATH(Queue{0U}, ".*");
+}
+
+TEST(QueueTest, push_pop)
+{
+  Queue queue{10U};
+  EXPECT_EQ(queue.size(), 0U);
+  EXPECT_EQ(queue.capacity(), 10U);
+  EXPECT_TRUE(queue.empty());
+
+  for (std::size_t i = 0U; i < queue.capacity(); ++i)
+  {
+    const Struct s{static_cast<float>(i + 1U), static_cast<std::int32_t>(i + 1U), static_cast<std::uint8_t>(i + 1U)};
+    queue.push(s);
+  }
+
+  EXPECT_EQ(queue.size(), queue.capacity());
+  EXPECT_EQ(queue.front(), (Struct{1.0F, 1, 1U}));
+  EXPECT_EQ(queue.back(), (Struct{10.0F, 10, 10U}));
+
+  EXPECT_DEATH(queue.push(Struct{11.0F, 11, static_cast<uint8_t>(11)}), ".*");
+
+  std::size_t i = 0U;
+  while (!queue.empty())
+  {
+    Struct s;
+    queue.pop(s);
+
+    EXPECT_EQ(
+        s, (Struct{static_cast<float>(i + 1U), static_cast<std::int32_t>(i + 1U), static_cast<std::uint8_t>(i + 1U)}));
+
+    ++i;
+  }
+
+  EXPECT_EQ(queue.size(), 0U);
+  EXPECT_EQ(queue.capacity(), 10U);
+  EXPECT_TRUE(queue.empty());
+
+  EXPECT_DEATH(queue.pop(), ".*");
+}
+
+TEST(QueueTest, emplace_pop)
+{
+  Queue queue{10U};
+  EXPECT_EQ(queue.size(), 0U);
+  EXPECT_EQ(queue.capacity(), 10U);
+  EXPECT_TRUE(queue.empty());
+
+  for (std::size_t i = 0U; i < queue.capacity(); ++i)
+  {
+    queue.emplace(static_cast<float>(i + 1U), static_cast<std::int32_t>(i + 1U), static_cast<std::uint8_t>(i + 1U));
+  }
+
+  EXPECT_EQ(queue.size(), queue.capacity());
+  EXPECT_EQ(queue.front(), (Struct{1.0F, 1, 1U}));
+  EXPECT_EQ(queue.back(), (Struct{10.0F, 10, 10U}));
+
+  EXPECT_DEATH(queue.emplace(11.0F, 11, static_cast<uint8_t>(11)), ".*");
+
+  std::size_t i = 0U;
+  while (!queue.empty())
+  {
+    Struct s;
+    queue.pop(s);
+
+    EXPECT_EQ(
+        s, (Struct{static_cast<float>(i + 1U), static_cast<std::int32_t>(i + 1U), static_cast<std::uint8_t>(i + 1U)}));
+
+    ++i;
+  }
+
+  EXPECT_EQ(queue.size(), 0U);
+  EXPECT_EQ(queue.capacity(), 10U);
+  EXPECT_TRUE(queue.empty());
+
+  EXPECT_DEATH(queue.pop(), ".*");
+}
