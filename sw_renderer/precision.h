9899c1abc758220e5200a5b40d559091defd2c33
Kirill Klochkov <759108+kklochkov@users.noreply.github.com>
2026-06-11 15:25:53 +0200

    use correct ULP

--- sw_renderer/precision.h
+++ sw_renderer/precision.h
@@ -12,15 +12,12 @@ namespace rtw::sw_renderer
 #ifdef RTW_USE_FIXED_POINT
 using single_precision = multiprecision::FixedPoint16; ///< Primary scalar type (fixed-point mode).
 using double_precision = multiprecision::FixedPoint32; ///< Extended scalar type (fixed-point mode).
-
-/// Unit of least precision (smallest representable increment above zero).
-constexpr inline single_precision ULP{1};
 #else
 using single_precision = float;  ///< Primary scalar type (floating-point mode).
 using double_precision = double; ///< Extended scalar type (floating-point mode).
+#endif
 
 /// Unit of least precision (machine epsilon for float).
-constexpr inline float ULP{std::numeric_limits<float>::epsilon()};
-#endif
+constexpr inline single_precision ULP{std::numeric_limits<single_precision>::epsilon()};
 
 } // namespace rtw::sw_renderer
